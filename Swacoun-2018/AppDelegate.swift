010cada2e6f1f62c0143c01728fca465b3d1c263
Marcel Weiher <marcel@metaobject.com>
2018-09-14 17:52:04 +0200

    Initial Commit

---
+++ Swacoun-2018/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swacoun-2018
+//
+//  Created by Marcel Weiher on 9/14/18.
+//  Copyright © 2018 metaobject. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swacoun-2018/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swacoun-2018
+//
+//  Created by Marcel Weiher on 9/14/18.
+//  Copyright © 2018 metaobject. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
