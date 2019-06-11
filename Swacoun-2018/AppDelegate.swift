981291d35fcb287a355cb6d768ecf45d4eb331f4
Marcel Weiher <marcel@metaobject.com>
2019-06-11 19:50:57 +0200

    all the stuff

--- Swacoun-2018/AppDelegate.swift
+++ Swacoun-2018/AppDelegate.swift
@@ -7,14 +7,70 @@
 //
 
 import Cocoa
+import MPWFoundation
+import ObjectiveXML
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var objects:[TestClass]=[]
+    
+    func createObjects() {
+        for i:Int32 in 1...1000000 {
+            objects.append(TestClass(hi: i, there: i*2, comment: "comment"))
+        }
+    }
+    
+    func writeJSONViaStream() -> Data {
+        NSLog("Stream Encode")
+        let data=NSMutableData()
+        let writer = MPWJSONWriter(target: data)!
+        writer.write( objects )
+        writer.close()
+        return data as Data
+    }
+    
+    func writeBPListViaStream() -> Data {
+        NSLog("BPList Stream Encode")
+        let data=NSMutableData()
+        let writer = MPWBinaryPListWriter(target: data)!
+        writer.write( objects )
+        writer.close()
+        return data as Data
+    }
+    
 
+    
+    func writeJSONCoder() -> Data {
+        NSLog("Swift Coding")
+        let coder=JSONEncoder()
+        let data=try! coder.encode(objects)
+        return data
+    }
+    func writeJSONSerialization() -> Data {
+        NSLog("NSJSONSerialization, create plist")
+        let plist=self.objects.map { $0.asDictionary() }
+        NSLog("NSJSONSerialization, encode the plist")
+        return try! JSONSerialization.data(withJSONObject: plist, options: [])
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSLog("will create")
+        createObjects()
+        let start =  Date.timeIntervalSinceReferenceDate;
+        let data = writeJSONViaStream()
+//      let data = writeJSONCoder()
+//        let data = writeBPListViaStream()
+//      let data = writeJSONSerialization()
+        let encodeTime = Date.timeIntervalSinceReferenceDate - start
+        NSLog("did encode in %g seconds, write",encodeTime)
+        let writStart = Date.timeIntervalSinceReferenceDate
+        try! data.write(to: URL(fileURLWithPath: "/tmp/swlist.json"), options: [])
+        let writeTime = Date.timeIntervalSinceReferenceDate - writStart
+        NSLog("did write in %g seconds",writeTime)
+        NSLog("total: %g seconds",encodeTime+writeTime)
+        NSLog("I/O: %g %%",writeTime/encodeTime * 100.0)
+        NSLog("CPU: %g %%",100 - (writeTime/encodeTime * 100.0))
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ Swacoun-2018/TestClass.swift
@@ -0,0 +1,41 @@
+//
+//  TestClass.swift
+//  Swacoun-2018
+//
+//  Created by Marcel Weiher on 9/14/18.
+//  Copyright © 2018 metaobject. All rights reserved.
+//
+
+import Foundation
+import ObjectiveXML
+
+@objc class TestClass: NSObject, Codable {
+    let hi:Int32
+    let there:Int32
+    let comment:String
+
+
+    init( hi:Int32 , there:Int32, comment:String ) {
+        self.hi=hi
+        self.there=there
+        self.comment=comment
+    }
+    
+    
+    public func asDictionary() -> [String:Any] {
+        return ["hi": hi,
+                "there": there,
+                "comment": comment]
+    }
+    
+    @objc override public func write(onJSONStream aStream: MPWJSONWriter!) {
+        aStream.writeDictionaryLike( "") { (writer) in
+            if let writer=writer {
+                writer.writeInteger(self.hi, forKey: "hi")
+                writer.writeInteger(self.there, forKey: "there")
+                writer.write(self.comment, forKey: "comment")
+            }
+        }
+    }
+
+}
